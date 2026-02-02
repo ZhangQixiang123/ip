4e8b2dee48c0bbfa4a0ecbd783105f87a57b06e6
ZhangQixiang123 <e1264831@u.nus.edu>
2026-02-02 22:27:07 +0800

    level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
@@ -11,6 +12,8 @@ public class Duke {
         System.out.println(" What can I do for you?");
         System.out.println(line);
 
+        ArrayList<String> ls = new ArrayList<>();
+
         while (true) {
             String input = scanner.nextLine();
             System.out.println(line);
@@ -18,9 +21,14 @@ public class Duke {
                 System.out.println(" Bye. Hope to see you again soon!");
                 System.out.println(line);
                 break;
+            } else if (input.equals("list")) {
+                for (int i = 0; i < ls.size(); i++) {
+                    System.out.println(i+1 + ". " + ls.get(i));
+                }
+            } else {
+                ls.add(input);
+                System.out.println("added: " + input);
             }
-            System.out.println(" " + input);
-            System.out.println(line);
         }
 
         scanner.close();
