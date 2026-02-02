9977741a102387153f51ff9f01da8fbd716d6998
ZhangQixiang123 <e1264831@u.nus.edu>
2026-02-02 22:21:24 +0800

    level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,28 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String name = "fzjjs";
-        System.out.println("Hello I'm " + name);
-        System.out.println("What can I do for you?");
-        System.out.println("\n");
-        System.out.println("Bye. Hope to see you again soon!");
+        String line = "____________________________________________________________";
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(line);
+        System.out.println(" Hello! I'm " + name);
+        System.out.println(" What can I do for you?");
+        System.out.println(line);
+
+        while (true) {
+            String input = scanner.nextLine();
+            System.out.println(line);
+            if (input.equals("bye")) {
+                System.out.println(" Bye. Hope to see you again soon!");
+                System.out.println(line);
+                break;
+            }
+            System.out.println(" " + input);
+            System.out.println(line);
+        }
+
+        scanner.close();
     }
 }
